a50c346501d02f4b67d4a95b576a3c6826adcc0c
Nicolas Mondain <nicolas.mondain@gmail.com>
2025-10-25 00:51:03 +0200

    feat(ui): tailwindcss components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
 
 export default defineConfig({
-  site: "https://nicolasmondain.github.io/ooo",
-  base: "/ooo/",
-  integrations: [tailwind(), sitemap()],
+  site: 'https://nicolasmondain.github.io/ooo',
+  base: '/ooo/',
+  integrations: [reacttailwind(), sitemap()],
   markdown: {
     smartypants: true,
+  },
+  build: {
+    format: 'directory', // ensures 404.html is at the root
   },
 });
